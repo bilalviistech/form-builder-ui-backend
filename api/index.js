e257ed1098eb29b3ea64c249f550a1a3fedae370
bilalviistech <bilalviistech@gmail.com>
2026-03-03 23:06:29 +0500

    adding one line

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import apiRoutes from '../routes/api'
+import apiRoutes from '../routes/api.js'
 
 const app = express();
 
